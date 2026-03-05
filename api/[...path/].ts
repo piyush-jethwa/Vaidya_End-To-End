7fb416edae398637c509a0bb7b5cbf931ec6b5c2
piyush-jethwa <piyushjethwa727@gmail.com>
2026-03-06 01:52:42 +0530

    Fix Vercel API routing to serverless handler

--- api/[...path/].ts
+++ api/[...path/].ts
@@ -2,9 +2,9 @@ import { createServer } from '../../server/index';
 
 export default async function handler(req: any, res: any) {
   const app = createServer();
-  
+
   // Handle the request through Express
-  return new Promise((resolve) => {
+  return new Promise<void>((resolve) => {
     app(req, res, () => {
       resolve();
     });
