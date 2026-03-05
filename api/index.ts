12677fa41d745d8a2b7aa3550cbea7c60594ebf6
piyush-jethwa <piyushjethwa727@gmail.com>
2026-03-06 02:22:57 +0530

    Fix Vercel API routing with proper function configuration

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+import { createServer } from '../server/index';
+
+const app = createServer();
+
+export default async function handler(req: any, res: any) {
+  // Let Express handle the request
+  return new Promise((resolve, reject) => {
+    app(req, res, (err?: any) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(undefined);
+      }
+    });
+  });
+}
