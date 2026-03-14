616d980691d4cb02b7090f1b96d06bb4ffc41d50
Piyush Jethwa <163643129+piyush-jethwa@users.noreply.github.com>
2026-03-14 10:41:32 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,14 @@
-import serverless from 'serverless-http';
 import { createServer } from '../server';
 
-export default serverless(createServer());
+export default async function handler(req: any, res: any) {
+  const app = createServer();
+  return new Promise<void>((resolve, reject) => {
+    app(req, res, (err?: any) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
